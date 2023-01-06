e03c9b64a3b57bd8995deb9b96cfe420aa6137d1
Nik_Voi <tivaipivka@gmail.com>
2023-01-06 20:09:18 +0300

    add, feat: new block, mobile nav

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,13 @@
-console.log('file 1');
+const menu = document.querySelector(".menu");
+const button = document.querySelector(".header__mobile");
+
+button.addEventListener("click", () =>{
+
+    if (menu.classList.toggle("active")){
+        menu.classList.add('active');
+        // body.classList.add('noscroll');
+    }else{
+        menu.classList.remove('active');
+        // body.classList.remove('noscroll');
+    }
+})
